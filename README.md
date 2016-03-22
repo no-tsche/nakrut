41307dc9409b52789728c66511146621b02d592c
no-tsche <no.tsche@protonmail.com>
2016-03-22 23:14:28 +0200

    initial functions

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Run 20 requests:
 `nakrut "proxy-list.txt" "http://url.that.needs/views" 20`
 
 Proxy-list file format:
-`192.168.1.100\t8080\n  
-192.168.1.101\t8081\n  
+`192.168.1.100\t8080\t\n  
+192.168.1.101\t8081\t\n  
 ...`
 
 ## Status
--- src/nakrut/core.clj
+++ src/nakrut/core.clj
@@ -2,4 +2,11 @@
   (:require [clojure.string :as str])
   (:require [clj-http.client :as client]))
 
+(defn get-proxies
+  [proxy-list-file]
+  (with-open [rdr (clojure.java.io/reader proxy-list-file)]
+    (reduce conj [] (line-seq rdr))))
 
+(defn -main
+  [proxy-list-file]
+  (get-proxies proxy-list-file))
