37d3d84bfb7173981bf0521a46024b10943601d5
no-tsche <no.tsche@protonmail.com>
2016-03-22 23:59:22 +0200

    some additional work

--- src/nakrut/core.clj
+++ src/nakrut/core.clj
@@ -2,11 +2,20 @@
   (:require [clojure.string :as str])
   (:require [clj-http.client :as client]))
 
+(defn open-file
+  "Attempts to open a file and complains if the file is not present."
+  [file-name]
+  (let [file-data (try
+                    (slurp file-name)
+                    (catch Exception e (println (.getMessage e))))]
+    file-data))
+
+
 (defn get-proxies
+  "Getting parsed vector of proxies"
   [proxy-list-file]
-  (with-open [rdr (clojure.java.io/reader proxy-list-file)]
-    (reduce conj [] (line-seq rdr))))
+  (str/split-lines proxy-list-file))
 
 (defn -main
   [proxy-list-file]
-  (get-proxies proxy-list-file))
+  (get-proxies (open-file proxy-list-file)))
