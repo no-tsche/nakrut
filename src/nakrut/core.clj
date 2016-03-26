57676873e94abfe4a36f046ab5b0354b57295a56
tsche <cvetnie.kolesa@gmail.com>
2016-03-26 14:20:45 +0200

    small refactoring

--- src/nakrut/core.clj
+++ src/nakrut/core.clj
@@ -10,15 +10,10 @@
                     (catch Exception e (println (.getMessage e))))]
     file-data))
 
-(defn parse-proxy-line
-  "Parses line so it's data formatted without tabs. Only ip and port."
-  [line]
-  (str/split line #"\t"))
-
 (defn get-proxies
   "Getting parsed list of proxies."
   [proxy-list-file]
-  (map parse-proxy-line (str/split-lines proxy-list-file)))
+  (map (fn [x] (str/split x #"\t")) (str/split-lines proxy-list-file)))
 
 (defn -main
   "For this moment this function can only transform .txt list of proxies into parsed list."
